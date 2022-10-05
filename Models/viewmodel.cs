ec87dbd82e3b31775934234c6fe15b4f0e6f37f5
u19059613 <u19059613@tuks.co.za>
2022-10-05 23:13:30 +0200

    Create viewmodel.cs
    Co-Authored-By: inf272marker <102524140+inf272marker@users.noreply.github.com>

---
+++ Models/viewmodel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u19059613_HW5.Models
+{
+    public class viewmodel
+    {
+        public Book Books { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}

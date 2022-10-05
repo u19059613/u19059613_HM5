0dc4ea8999bf3ed3056cece0dba62a480e43b67e
u19059613 <u19059613@tuks.co.za>
2022-10-05 23:09:59 +0200

    Create HomeController.cs
    Co-Authored-By: inf272marker <102524140+inf272marker@users.noreply.github.com>

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using u19059613_HW5.Models;
+using u19059613_HW5.Models.Dataservice;
+
+
+
+namespace u19059613_HW5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private DataService dataService = DataService.getInstance();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult BookView()
+        {
+            List<Books> book = dataService.getBooks();
+            return View(book);
+        }
+
+        public ActionResult BookDetailsView(int id)
+        {
+            List<booksdetails> bookdetails = dataService.getBorrows(id);
+            return View(bookdetails);
+        }
+
+        public ActionResult ViewStudent()
+        {
+            List<Students> studentDetails = dataService.getStudents();
+            return View(studentDetails);
+        }
+    }
+}

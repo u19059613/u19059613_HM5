31d518df9f3377d4b7a70fdfe7266b95c5e1f77d
u19059613 <u19059613@tuks.co.za>
2022-10-05 23:13:44 +0200

    Create booksdetails.cs
    Co-Authored-By: inf272marker <102524140+inf272marker@users.noreply.github.com>

---
+++ Models/booksdetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u19059613_HW5.Models
+{
+    public class booksdetails
+    {
+        public int BorrowID { get; set; }
+        public DateTime Take_Date { get; set; }
+        public DateTime Brought_Date { get; set; }
+        public Students Borrow_By { get; set; }
+
+    }
+}

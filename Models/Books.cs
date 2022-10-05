114c09b93243f706015aa417f93eb123e8a4d49c
u19059613 <u19059613@tuks.co.za>
2022-10-05 23:13:49 +0200

    Create Books.cs
    Co-Authored-By: inf272marker <102524140+inf272marker@users.noreply.github.com>

---
+++ Models/Books.cs
@@ -0,0 +1,16 @@
+﻿namespace u19059613_HW5.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string bName { get; set; }
+        public string bAuthor { get; set; }
+        public string bType { get; set; }
+        public int bPage_Count { get; set; }
+        public int bPoints { get; set; }
+        public string bStatus { get; set; }
+
+
+
+    }
+}

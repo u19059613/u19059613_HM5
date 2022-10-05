8d81ad8cc2c7000b4afd4f9f15f3b502231fde92
u19059613 <u19059613@tuks.co.za>
2022-10-05 23:13:34 +0200

    Create Students.cs
    Co-Authored-By: inf272marker <102524140+inf272marker@users.noreply.github.com>

---
+++ Models/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u19059613_HW5.Models
+{
+    public class Students
+    {
+        public int StudentID { get; set; }
+        public string Sname { get; set; }
+        public string Ssurname { get; set; }
+        public string Sclass { get; set; }
+        public int Spoints { get; set; }
+       
+    }
+}
